953467fa393c6ba5d604eb7d6a5e8bdb07958b7f
Zach 🦀🌲 <zachary.jay.crabtree@gmail.com>
2018-12-30 15:48:47 +0100

    Create main character entity
    The collision logic isn't perfect or modular, but it's good enough for a
    first go around.
    
    https://github.com/zakattak/Timbre/issues/2

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	
+	public float speed = 5f;
+	private BoxCollider2D boxCollider;
+	private ContactFilter2D contactFilter = new ContactFilter2D().NoFilter();
+
+	void Awake () {
+		boxCollider = GetComponent<BoxCollider2D>();
+	}
+
+	void Update () {
+		Vector3 previousPos = transform.position;
+		Vector3 movePos = transform.position;
+
+		// TODO: Use UniRX if the project gets bigger
+		if (Input.GetKey ("w")) {
+			movePos.y += speed * Time.deltaTime;
+		}
+		if (Input.GetKey ("s")) {
+			movePos.y -= speed * Time.deltaTime;
+		}
+		if (Input.GetKey ("d")) {
+			movePos.x += speed * Time.deltaTime;
+		}
+		if (Input.GetKey ("a")) {
+			movePos.x -= speed * Time.deltaTime;
+		}
+			
+		transform.position = movePos;
+
+		int numColliders = 10;
+		Collider2D[] colliders = new Collider2D[numColliders];
+		int colliderCount = boxCollider.OverlapCollider(contactFilter, colliders);
+
+		if (colliderCount > 0) {
+			for (int i = 0; i < colliderCount; i++) {
+				if (colliders[i].gameObject.tag == "Wall") {
+					transform.position = previousPos;
+				}
+			}
+		}
+	}
+}
