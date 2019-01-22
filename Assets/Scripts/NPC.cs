c86dd6bf9612beea03fdeac7dda59eb902478e39
Zach 🦀🌲 <zachary.jay.crabtree@gmail.com>
2019-01-22 20:14:03 +0100

    Remove unneeded NPC code

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -5,26 +5,13 @@ using UnityEngine;
 public class NPC : MonoBehaviour {
 
 	public string message;
-	private BoxCollider2D boxCollider;
-	private ContactFilter2D contactFilter = new ContactFilter2D().NoFilter();
 
 	void Awake () {
-		boxCollider = GetComponent<BoxCollider2D>();
+		
 	}
+
 	void Update () {
-		int numColliders = 10;
-		Collider2D[] colliders = new Collider2D[numColliders];
-		int colliderCount = boxCollider.OverlapCollider(contactFilter, colliders);
-
-		//print(colliderCount);
-
-		if (colliderCount > 0) {
-			for (int i = 0; i < colliderCount; i++) {
-				if (colliders[i].gameObject.tag == "Player") {
-					//print(message);
-				}
-			}
-		}
+		
 	}
 
 	void PlayerInteract() {
