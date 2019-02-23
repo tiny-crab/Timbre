6b6beb355e2d3a3f469118d22ff2f8d5a18af9ee
Zach 🦀🌲 <zachary.jay.crabtree@gmail.com>
2019-02-23 15:15:26 +0100

    Add tile collider logic
    Now you can click on the grid and you'll see a log representing which
    tile was clicked. Yay.

--- Assets/GridStage.cs
+++ Assets/GridStage.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class GridStage : MonoBehaviour {
 
-	public int[,] grid = new int[6,10];
-	public SpriteRenderer tile;
+	public GameObject[,] grid = new GameObject[6,6];
+	public GameObject tile;
 
 	// Use this for initialization
 	void Start () {
 		Vector3 origin = new Vector3(0, 0, 0);
 		// assuming tiles are squares
-		float tileWidth = tile.size.x;
+		float tileWidth = tile.GetComponent<SpriteRenderer>().size.x;
 		Vector2 gridSize = new Vector2(tileWidth * grid.GetLength(0), tileWidth * grid.GetLength(1));
 
 		print(gridSize.x);
@@ -19,21 +19,31 @@ public class GridStage : MonoBehaviour {
 
 		Vector3 topLeft = new Vector3(origin.x - (gridSize.x / 4), origin.y + (gridSize.y / 4), origin.z);
 
+		var index = 0;
 		for (int i = 0; i < grid.GetLength(0); i++) {
 			for (int j = 0; j < grid.GetLength(1); j++) {
-				Instantiate(tile, topLeft, Quaternion.identity);
+				grid[i,j] = Instantiate(tile, topLeft, Quaternion.identity);
+				index++;
+				grid[i,j].name = string.Format("I am Tile #{0}", index);
 				topLeft.x += tileWidth / 2;
 			}
 			topLeft.x = origin.x - (gridSize.x / 4);
 			topLeft.y -= tileWidth / 2;
 		}
-
-//		Instantiate(tile, topLeft, Quaternion.identity);
-//		Instantiate(tile, new Vector3(origin.x + (tileWidth / 2), origin.y, origin.z), Quaternion.identity);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetMouseButtonDown(0)) {
+			Vector2 mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			RaycastHit2D hitInfo = Physics2D.Raycast(mouseWorldPosition, Vector2.zero);
+			GameObject mouseTile = hitInfo.collider.gameObject;
+			print(string.Format(
+				"{0} at {1}, {2}",
+				mouseTile.name,
+				mouseTile.transform.position.x,
+				mouseTile.transform.position.y
+			));
+		}
 	}
 }
