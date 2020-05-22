ed4aaeffc458da0db754ff3480ce761f4b49300a
Zach 🦀🌲 <zachary.jay.crabtree@gmail.com>
2020-05-22 12:15:39 -0700

    Create initial Gauntlet FSM with ThreadCustomization stage

--- Assets/Scripts/Gauntlet.cs
+++ Assets/Scripts/Gauntlet.cs
@@ -1,13 +1,17 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Gauntlet : MonoBehaviour
 {
     GridSystem gridSystem;
+    GauntletState currentState;
+
+    GameObject ethreadMenu;
 
     void Awake () {
         gridSystem = GameObject.Find("GridSystem").GetComponent<GridSystem>();
+        ethreadMenu = GameObject.Find("EthreadMenu");
     }
 
     void Start () {
@@ -19,14 +23,14 @@ public class Gauntlet : MonoBehaviour
             },
             enemiesToSpawn: GenerateRandomEnemies()
         );
+
+        ethreadMenu.SetActive(false);
+
+        currentState = new Combat(this);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (gridSystem.allAlliesDefeated || gridSystem.allEnemiesDefeated) {
-            EndGauntlet();
-        }
+    void Update() {
+        currentState.UpdateState();
     }
 
     private void EndGauntlet() {
@@ -43,5 +47,62 @@ public class Gauntlet : MonoBehaviour
         return new List<KeyValuePair<GameObject, Vector2>>() {
             pair,
         };
+    }
+
+    private void ChangeState(Type newStateType) {
+        if (newStateType.IsSubclassOf(typeof(GauntletState))) {
+            var oldState = currentState;
+            oldState.EndState();
+            var newState = (GauntletState) Activator.CreateInstance(newStateType, new object[] { this });
+            newState.StartState();
+            currentState = newState;
+        }
+    }
+
+    public class GauntletState {
+        public Gauntlet parent;
+        public GauntletState(Gauntlet parent) { this.parent = parent; }
+
+        public virtual void StartState() {}
+        public virtual void UpdateState() {}
+        public virtual void EndState() {}
+    }
+
+    public class Combat : GauntletState {
+        public Combat(Gauntlet parent) : base(parent) {}
+
+        public override void StartState() {}
+        public override void UpdateState() {
+            if (parent.gridSystem.allAlliesDefeated || parent.gridSystem.allEnemiesDefeated) {
+                parent.ChangeState(typeof(ThreadCustomization));
+            }
+        }
+        public override void EndState() {}
+    }
+
+    public class ThreadCustomization : GauntletState {
+        public ThreadCustomization(Gauntlet parent) : base(parent) {}
+
+        public override void StartState() {
+            parent.ethreadMenu.SetActive(true);
+        }
+        public override void UpdateState() {
+            if (Input.GetKeyDown(KeyCode.Escape)) {
+                parent.ChangeState(typeof(Complete));
+            }
+        }
+        public override void EndState() {
+            parent.ethreadMenu.SetActive(false);
+        }
+    }
+
+    public class Complete : GauntletState {
+        public Complete(Gauntlet parent) : base(parent) {}
+
+        public override void StartState() {
+            parent.EndGauntlet();
+        }
+        public override void UpdateState() {}
+        public override void EndState() {}
     }
 }
